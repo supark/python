f63edca9b9fbde5a13d9a45813e782caf99d1c6c
Su Park <andjs1012@gmail.com>
2016-01-02 18:10:38 -0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,3 +1,11 @@
+"""Write a python program that takes a key(a number from 1 to 26)an input file name, and an output file name from the 
+command line and uses the key to encrypt it with a ceaser cipher.  A Ceaser cipher performs modular addition of the key and 
+the letter.  If the key is 1 then a->b, b->c, ..., y->z, z->a.  Ignore all the things that aren't the letters from a to z
+(simply pass them through) and you will want to put the letters into lower case.  Use the file message1.txt and a key of 7 to
+encrypt it.
+"""
+
+
 #!/usr/bin/python
 
 key = input("Please type a key : ")
